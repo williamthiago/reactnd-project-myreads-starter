a5b28873570f04622e53d3607d3d51f125ec7511
William Thiago <williamthiago@gmail.com>
2017-09-10 15:44:47 -0300

    feat: Add debounce time before each search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,42 +1,40 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { Debounce } from 'react-throttle';
 
 import * as BooksAPI from '../BooksAPI'
 import Books from './Books'
 
 class Search extends Component {
   state = {
-    query: '',
     result: []
   }
 
   async handleSearch(query) {
-    query = query.trim()
-    this.setState({ query })
-    
     if (!query) {
       return
     }
-    
+
     let result = await BooksAPI.search(query, 10)
 
     this.setState({ result })
   }
 
   render() {
-    const { query, result: books } = this.state
+    const { result: books } = this.state
 
     return (
       <div className="search-books">
         <div className="search-books-bar">
           <Link className="close-search" to="/">Close</Link>
           <div className="search-books-input-wrapper">
-            <input 
-              type="text" 
-              placeholder="Search by title or author"
-              value={query}
-              onChange={(event) => this.handleSearch(event.target.value)}
-            />
+            <Debounce time="300" handler="onChange">
+              <input 
+                type="text" 
+                placeholder="Search by title or author"
+                onChange={(event) => this.handleSearch(event.target.value)}
+              />
+            </Debounce>
           </div>
         </div>
         <div className="search-books-results">
